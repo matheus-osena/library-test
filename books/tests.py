fe362fc94bc02fdf365817149d083af44aeb8782
Matheus Sena <matheus_osena@hotmail.com>
2023-05-18 17:34:58 -0300

    library project test

---
+++ books/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Books
+
+# Create your tests here.
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Books.objects.create(
+            title = "A good title",
+            subtitle = "An excellent subtitle",
+            author = "Tom Christie",
+            isbn = '1234567891023',
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, 'A good title')
+        self.assertEqual(self.book.subtitle, 'An excellent subtitle')
+        self.assertEqual(self.book.author, 'Tom Christie')
+        self.assertEqual(self.book.isbn, '1234567891023')
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "excellent subtitle")
+        self.assertTemplateUsed(response, 'books/books_list.html')
---
+++ books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+# Create your views here.
+from .models import Books
+
+class BookListView(ListView):
+    model = Books
+    template_name = "books_list.html"
